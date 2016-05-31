60615941a168908d40498d31a92f0c6667836f0c
ryanmims <rm7439@stu.armstrong.edu>
2016-05-31 11:22:23 -0400

    Changed everything to JavaFX
    -- will eventually make the GUI look better.

--- src/edu/armstrong/robotics/Compiler.java
+++ src/edu/armstrong/robotics/Compiler.java
@@ -1,138 +1,114 @@
 package edu.armstrong.robotics;
 
-import java.awt.BorderLayout;
-import java.awt.Button;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.TextArea;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-import javax.swing.JFrame;
+public class Compiler extends Application {
 
-public class Compiler extends JFrame {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-	public static ArrayList<String> compile(String text) {
-		String[] tokens = text.split("( )|(,)|[(]|[)]|(;)");
+    public static ArrayList<String> compile(String text) {
+        String[] tokens = text.split("( )|(,)|[(]|[)]|(;)");
 
-		// REMOVE ALL WHITESPACE
-		ArrayList<String> temp = new ArrayList<>();
-		for (int i = 0; i < tokens.length; i++) {
-			if (!tokens[i].isEmpty()) {
-				temp.add(tokens[i]);
-				System.out.println(tokens[i]);
-			}
-		}
-		tokens = new String[temp.size()];
-		for (int i = 0; i < temp.size(); i++)
-			tokens[i] = temp.get(i);
+        // REMOVE ALL WHITESPACE
+        ArrayList<String> temp = new ArrayList<>();
+        for (int i = 0; i < tokens.length; i++) {
+            if (!tokens[i].isEmpty()) {
+                temp.add(tokens[i]);
+                System.out.println(tokens[i]);
+            }
+        }
+        tokens = new String[temp.size()];
+        for (int i = 0; i < temp.size(); i++) {
+            tokens[i] = temp.get(i);
+        }
 
-		ArrayList<String> result = new ArrayList<>();
+        ArrayList<String> result = new ArrayList<>();
 
-		for (int i = 0; i < tokens.length; i++) {
+        for (int i = 0; i < tokens.length; i++) {
+            try {
 
-			try {
+                if (tokens[i].toLowerCase().contains("point")) {
+                    result.add(new String("gotoPoint("
+                            + Integer.parseInt(tokens[i + 1]) + ", "
+                            + Integer.parseInt(tokens[i + 2]) + ");"));
+                } else if (tokens[i].toLowerCase().contains("edge")) {
+                    result.add(new String("gotoEdge("
+                            + Integer.parseInt(tokens[i + 1]) + ");"));
+                } else if (tokens[i].toLowerCase().contains("corner")) {
+                    result.add(new String("gotoCorner("
+                            + Integer.parseInt(tokens[i + 1]) + ");"));
+                } else if (tokens[i].toLowerCase().contains("move")) {
+                    if (tokens[i + 1].toLowerCase().startsWith("n")) {
+                        result.add(new String("move(NORTH,"
+                                + Integer.parseInt(tokens[i + 2]) + ");"));
+                    } else if (tokens[i + 1].toLowerCase().startsWith("s")) {
+                        result.add(new String("move(SOUTH,"
+                                + Integer.parseInt(tokens[i + 2]) + ");"));
+                    } else if (tokens[i + 1].toLowerCase().startsWith("e")) {
+                        result.add(new String("move(EAST,"
+                                + Integer.parseInt(tokens[i + 2]) + ");"));
+                    } else if (tokens[i + 1].toLowerCase().startsWith("w")) {
+                        result.add(new String("move(WEST,"
+                                + Integer.parseInt(tokens[i + 2]) + ");"));
+                    } else {
+                        result.add(new String("move(NORTH,"
+                                + Integer.parseInt(tokens[i + 2]) + ");"));
+                    }
+                } else if (tokens[i].toLowerCase().contains("face")) {
+                    if (tokens[i + 1].toLowerCase().startsWith("n")) {
+                        result.add(new String("changeDir(NORTH);"));
+                    } else if (tokens[i + 1].toLowerCase().startsWith("s")) {
+                        result.add(new String("changeDir(SOUTH);"));
+                    } else if (tokens[i + 1].toLowerCase().startsWith("e")) {
+                        result.add(new String("changeDir(EAST);"));
+                    } else if (tokens[i + 1].toLowerCase().startsWith("w")) {
+                        result.add(new String("changeDir(WEST);"));
+                    } else {
+                        result.add(new String("changeDir(NORTH);"));
+                    }
+                } else {
+                    // skip
+                }
+            } catch (NumberFormatException e) {
+            }
+        }
+        return result;
+    }
 
-				if (tokens[i].toLowerCase().contains("point")) {
-					result.add(new String("gotoPoint("
-							+ Integer.parseInt(tokens[i + 1]) + ", "
-							+ Integer.parseInt(tokens[i + 2]) + ");"));
-				} else if (tokens[i].toLowerCase().contains("edge")) {
-					result.add(new String("gotoEdge("
-							+ Integer.parseInt(tokens[i + 1]) + ");"));
-				} else if (tokens[i].toLowerCase().contains("corner")) {
-					result.add(new String("gotoCorner("
-							+ Integer.parseInt(tokens[i + 1]) + ");"));
-				} else if (tokens[i].toLowerCase().contains("move")) {
-					if (tokens[i + 1].toLowerCase().startsWith("n")) {
-						result.add(new String("move(NORTH,"
-								+ Integer.parseInt(tokens[i + 2]) + ");"));
-					} else if (tokens[i + 1].toLowerCase().startsWith("s")) {
-						result.add(new String("move(SOUTH,"
-								+ Integer.parseInt(tokens[i + 2]) + ");"));
-					} else if (tokens[i + 1].toLowerCase().startsWith("e")) {
-						result.add(new String("move(EAST,"
-								+ Integer.parseInt(tokens[i + 2]) + ");"));
-					} else if (tokens[i + 1].toLowerCase().startsWith("w")) {
-						result.add(new String("move(WEST,"
-								+ Integer.parseInt(tokens[i + 2]) + ");"));
-					} else {
-						result.add(new String("move(NORTH,"
-								+ Integer.parseInt(tokens[i + 2]) + ");"));
-					}
-				} else if (tokens[i].toLowerCase().contains("face")) {
-					if (tokens[i + 1].toLowerCase().startsWith("n")) {
-						result.add(new String("changeDir(NORTH);"));
-					} else if (tokens[i + 1].toLowerCase().startsWith("s")) {
-						result.add(new String("changeDir(SOUTH);"));
-					} else if (tokens[i + 1].toLowerCase().startsWith("e")) {
-						result.add(new String("changeDir(EAST);"));
-					} else if (tokens[i + 1].toLowerCase().startsWith("w")) {
-						result.add(new String("changeDir(WEST);"));
-					} else {
-						result.add(new String("changeDir(NORTH);"));
-					}
-				} else {
-					// skip
-				}
-			} catch (NumberFormatException e) {
-			}
-		}
-		return result;
-	}
+    public void start(Stage primary) {
+        primary.setTitle("Robotics Compiler");
 
-	// TODO: check first to see if the user's syntax is correct
-	// if it isn't warn them about it
-	//
-	// Also perhaps add something that tells them
-	// whenever they're going out of bounds
+        TextArea script = new TextArea();
 
-	private String isCorrect(String text) {
-		return null;
-	}
+        Button compile = new Button();
+        compile.setText("Compile");
+        compile.setOnAction(new EventHandler<ActionEvent>() {
 
-	public Compiler() {
+            public void handle(ActionEvent event) {
+                System.out.println(compile(script.getText()));
+            }
+        });
 
-		super("Robotics Compiler");
+        BorderPane root = new BorderPane();
+        root.setCenter(script);
+        root.setBottom(compile);
+        primary.setScene(new Scene(root, 720, 480));
+        primary.show();
 
-		// syn is the user script
-		TextArea syn = new TextArea();
-		// fin is the compiled script
-		TextArea fin = new TextArea();
-		fin.setBackground(Color.LIGHT_GRAY);
-		fin.setPreferredSize(new Dimension(250, 250));
-		fin.setEditable(false);
-
-		Button compile = new Button("Compile");
-		compile.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				ArrayList<String> a = compile(syn.getText());
-				System.out.println(a);
-				String result = "// start main code";
-				for (String s : a) {
-					result = result + "\n" + s;
-				}
-				fin.setText(result);
-			}
-
-		});
-
-		add(BorderLayout.CENTER, syn);
-		add(BorderLayout.SOUTH, compile);
-		add(BorderLayout.EAST, fin);
-
-	}
-
-	public static void main(String[] args) {
-
-		Compiler com = new Compiler();
-		com.setSize(720, 480);
-		com.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		com.setLocationRelativeTo(null);
-		com.setVisible(true);
-
-	}
+    }
 }
+
